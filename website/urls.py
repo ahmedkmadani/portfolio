1dd779e87ab1bce51bddadb2cf2a52a29f9f2694
ahmedkmadani <ahmedkamal543@gmail.com>
2023-04-22 04:01:25 +0300

    add the rest of view

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views.index import index
-from .views.resume import resume
+
+from website.views import index, resume, portfolio, blog, contact
 
 urlpatterns = [
-    path("", index, name="index"),
-    path(r'resume', resume, name="resume"),
+    path("", index.index, name="index"),
+    path(r'resume', resume.resume, name="resume"),
+    path(r'portfolio', portfolio.portfolio, name="portfolio"),
+    path(r'blog', blog.blog, name="blog"),
+    path(r'contact', contact.contact, name="contact"),
 ]
