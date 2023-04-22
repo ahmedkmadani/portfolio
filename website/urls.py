88d30794a4b766c322025138328e4bdc2a968eb8
ahmedkmadani <ahmedkamal543@gmail.com>
2023-04-22 03:42:11 +0300

    add fonts

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views.index import index
+from .views.resume import resume
 
 urlpatterns = [
     path("", index, name="index"),
+    path(r'resume', resume, name="resume"),
 ]
