e91ca31b3d10c8a8057a110b9b9d8896533958e6
ahmedkmadani <ahmedkamal543@gmail.com>
2023-04-23 14:04:30 +0300

    add .env

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r&x+_n3a(jgm0$#*tftd3po=(=-lr8p6w3p(sbvh1ggmi)ye-f"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
